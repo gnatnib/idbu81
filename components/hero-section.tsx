909655481a36e9219ce067bd2b373a1905cd9cd5
Bintang Syafrian Rizal <117698972+gnatnib@users.noreply.github.com>
2025-08-26 18:09:30 +0700

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -17,7 +17,7 @@ export default function HeroSection() {
       <div
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: "url('/images/hero-bg.png')",
+          backgroundImage: "url('/images/bgkhas.png')",
         }}
       >
         <div className="absolute inset-0 bg-black/60" />
